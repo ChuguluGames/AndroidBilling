0fd6f2f557e56c65fbfc9665535ea29e60690b99
davidbillamboz <david@chugulu.com>
2012-01-06 15:01:33 +0100

    delete bin dir

--- android/bin/classes.dex.d
+++ android/bin/classes.dex.d
@@ -1,10 +1,2 @@
 /Users/davidbillamboz/Projects/AndroidBilling/android/bin/classes.dex : \
-/Users/davidbillamboz/Projects/AndroidBilling/android/bin/classes/com/phonegap/androidbilling/AndroidBilling.class \
-/Users/davidbillamboz/Projects/AndroidBilling/android/bin/classes/com/phonegap/androidbilling/R$attr.class \
-/Users/davidbillamboz/Projects/AndroidBilling/android/bin/classes/com/phonegap/androidbilling/R$drawable.class \
-/Users/davidbillamboz/Projects/AndroidBilling/android/bin/classes/com/phonegap/androidbilling/R$layout.class \
-/Users/davidbillamboz/Projects/AndroidBilling/android/bin/classes/com/phonegap/androidbilling/R$string.class \
-/Users/davidbillamboz/Projects/AndroidBilling/android/bin/classes/com/phonegap/androidbilling/R$xml.class \
-/Users/davidbillamboz/Projects/AndroidBilling/android/bin/classes/com/phonegap/androidbilling/R.class \
-/Users/davidbillamboz/Projects/AndroidBilling/android/bin/classes/com/phonegap/store/StorePlugin.class \
 /Users/davidbillamboz/Projects/AndroidBilling/android/libs/phonegap-1.3.0.jar \
