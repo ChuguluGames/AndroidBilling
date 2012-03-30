a885d4a6c5867ee7ae079fae5d1b9df69d3ec1c4
Skunk <skunk@dreamwall.fr>
2012-03-30 12:18:53 +0200

    handle error/exceptions

--- android/src/com/phonegap/store/StoreDelegate.java
+++ android/src/com/phonegap/store/StoreDelegate.java
@@ -1,10 +1,15 @@
 package com.phonegap.store;
 
 import android.util.Log;
+import com.billing.BillingService.RequestPurchase;
 import com.phonegap.api.PluginResult.Status;
 import com.phonegap.api.PluginResult;
 
 import com.phonegap.store.StoreConstants.Errors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class StoreDelegate {
     private static final String TAG = "StoreDelegate";
@@ -17,21 +22,40 @@ public class StoreDelegate {
         mCallbackID = callbackID;
     }
     
-    public void onPurchaseSuccess() {
+    public void onPurchaseSuccess(RequestPurchase request) {
         Log.d(TAG, "onPurchaseSuccess");
+        
         PluginResult result = new PluginResult(Status.OK);
         mPlugin.success(result, mCallbackID);
     }
     
-    public void onPurchaseCanceled() {
+    public void onPurchaseCanceled(RequestPurchase request) {
         Log.d(TAG, "onPurchaseCanceled");
-        PluginResult result = new PluginResult(Status.ERROR);
-        mPlugin.success(result, mCallbackID);
+        PluginResult result;
+        JSONObject error = new JSONObject();
+        try {
+            error.put("code", Errors.ON_UNKNOWN_ERROR);
+            error.put("request", Errors.ON_UNKNOWN_ERROR);
+            result = new PluginResult(Status.ERROR, error);
+        } catch (JSONException ex) {
+            result = new PluginResult(Status.JSON_EXCEPTION);
+        }
+        
+        mPlugin.error(result, mCallbackID);
     }
     
-    public void onPurchaseError() {
+    public void onPurchaseError(RequestPurchase request) {
         Log.d(TAG, "onPurchaseError");
-        PluginResult result = new PluginResult(Status.ERROR);
-        mPlugin.success(result, mCallbackID);
+        PluginResult result;
+        JSONObject error = new JSONObject();
+        try {
+            error.put("code", Errors.ON_UNKNOWN_ERROR);
+            error.put("request", Errors.ON_UNKNOWN_ERROR);
+            result = new PluginResult(Status.ERROR, error);
+        } catch (JSONException ex) {
+            result = new PluginResult(Status.JSON_EXCEPTION);
+        }
+        
+        mPlugin.error(result, mCallbackID);
     }    
 }
